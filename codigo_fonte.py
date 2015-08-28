630e009f0a71f178b33b2917a0cc2c18a2cc6daf
Silvio José Pereira dos Santos <silvio.jose.pereira.santos@live.com>
2015-08-28 16:55:37 -0300

    Adicionado pincel espesso e função de salvar imagem desenhada
    Adicionado pincel espesso e função de salvar imagem desenhada

--- codigo_fonte.py
+++ codigo_fonte.py
@@ -1,6 +1,7 @@
 # coding: utf8
 import pygame
 from pygame.locals import *
+from random import randint
 pygame.init()
 
 # tela
@@ -15,6 +16,7 @@ running = 1
 color = 255, 255, 255
 x = y = 0
 startPos=(0,0)
+desenhar = 1
 
 #variáveis de texto
 texto = pygame.font.SysFont('comic sans ms', 15)
@@ -33,7 +35,7 @@ background.fill((255,255,255))
 while running:
 
     #imagens
-    img_lapis = pygame.image.load("lapis.jpg")
+    img_lapis = pygame.image.load("img/lapis.jpg")
     screen.blit(img_lapis, (0, 0))
 
     #seletores de cor
@@ -46,11 +48,11 @@ while running:
     pygame.draw.rect(screen, (0, 0, 255), [140, 0, 20, 20])
     pygame.draw.rect(screen, (0, 255, 255), [160, 0, 20, 20])
 
-    #borracha
+    #borracha_label
     borracha = texto.render("Borracha", True, (255, 255, 255))
     screen.blit(borracha, (730,0))
 
-    #limpando tela
+    #limpando tela_label
     clear = texto.render("Limpar Tela", True, (255, 255, 255))
     screen.blit(clear, (620,0))
 
@@ -106,19 +108,46 @@ while running:
         background.fill((255,255,255))
 
     #borracha
-    elif x > 730 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+    if x > 730 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+        desenhar = 2
         line_color = 255, 255, 255
-        line_size = 20
-    
+
+    #escolher opções
+    pressionado = pygame.key.get_pressed()
+    if pressionado[K_l]:
+        desenhar = 1
+    elif pressionado[K_p]:
+        desenhar = 2
+
+    #salvar imagem
+    random = randint(0,300)
+    if pressionado[K_s]:
+        pygame.time.wait(2)
+        salvar = pygame.image.save(background,'imagens_salvas/image_%d.png' % random)
+
     #desenhar
-    for i in pygame.event.get():    
-        if i.type == pygame.MOUSEMOTION:
-            x,y = i.pos[0],(i.pos[1]-22)
-            endPos = [x,y]
-            if pygame.mouse.get_pressed()==(1,0,0):
-                pygame.draw.line(background, line_color, startPos, endPos, line_size)
-            startPos=endPos    
-    screen.blit(background,(0,22))
+
+    #lapis
+    if desenhar == 1:    
+        for i in pygame.event.get():    
+            if i.type == pygame.MOUSEMOTION:
+                x,y = i.pos[0],(i.pos[1]-22)
+                endPos = [x,y]
+                if pygame.mouse.get_pressed()==(1,0,0):
+                    pygame.draw.line(background, line_color, startPos, endPos, line_size)
+                startPos=endPos    
+        screen.blit(background,(0,22))
+
+    #pincel
+    elif desenhar == 2:
+        for i in pygame.event.get():    
+            if i.type == pygame.MOUSEMOTION:
+                x,y = i.pos[0],(i.pos[1]-22)
+                endPos = [x,y]
+                if pygame.mouse.get_pressed()==(1,0,0):
+                    pygame.draw.circle(background, line_color, (x,y), 20, 0)
+                startPos=endPos    
+        screen.blit(background,(0,22))
     
     #fechar a aplicação
     for i in pygame.event.get():
