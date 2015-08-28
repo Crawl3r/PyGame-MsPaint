96b2617d89c04fee38f93df8fe4481ba48482d5f
Silvio José Pereira dos Santos <silvio.jose.pereira.santos@live.com>
2015-08-28 00:27:46 -0300

    Adicionadas cores
    Adicionadas mais 5 cores e cor preta movida para o início da paleta.

--- codigo_fonte.py
+++ codigo_fonte.py
@@ -34,12 +34,17 @@ while running:
 
     #imagens
     img_lapis = pygame.image.load("lapis.jpg")
-    screen.blit(img_lapis, (60, 0))
+    screen.blit(img_lapis, (0, 0))
 
     #seletores de cor
-    pygame.draw.rect(screen, (255, 0, 0), [0, 0, 20, 20])
-    pygame.draw.rect(screen, (0, 255, 0), [20, 0, 20, 20])
-    pygame.draw.rect(screen, (0, 0, 255), [40, 0, 20, 20])
+    pygame.draw.rect(screen, (255, 0, 0), [20, 0, 20, 20])
+    pygame.draw.rect(screen, (255, 165, 0), [40, 0, 20, 20])
+    pygame.draw.rect(screen, (255, 255, 0), [60, 0, 20, 20])
+    pygame.draw.rect(screen, (177, 255, 47), [80, 0, 20, 20])
+    pygame.draw.rect(screen, (0, 255, 0), [100, 0, 20, 20])
+    pygame.draw.rect(screen, (46, 139, 87), [120, 0, 20, 20])
+    pygame.draw.rect(screen, (0, 0, 255), [140, 0, 20, 20])
+    pygame.draw.rect(screen, (0, 255, 255), [160, 0, 20, 20])
 
     #borracha
     borracha = texto.render("Borracha", True, (255, 255, 255))
@@ -52,23 +57,48 @@ while running:
     #troca de cores
 
     #preto
-    if x > 60 and x < 80 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+    if  x < 20 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
         line_color = 0, 0, 0
         line_size = 5
 
     #vermelho
-    if x < 20 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+    elif x > 20 and x < 40 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
         line_color = 255, 0, 0
         line_size = 5
 
+    #laranja
+    elif x > 40 and x < 60 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+        line_color = 255, 165, 0
+        line_size = 5
+
+    #amarelo
+    elif x > 60 and x < 80 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+        line_color = 255, 255, 0
+        line_size = 5
+
+    #meio verde
+    elif x > 80 and x < 100 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+        line_color = 177, 255, 47
+        line_size = 5
+
     #verde
-    elif x > 20 and x < 40 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+    elif x > 100 and x < 120 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
         line_color = 0, 255, 0
         line_size = 5
 
+    #meio azul
+    elif x > 120 and x < 140 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+        line_color = 46, 139, 87
+        line_size = 5
+
     #azul
-    elif x > 40 and x < 60 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+    elif x > 140 and x < 160 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
         line_color = 0, 0, 255
+        line_size = 5
+
+    #azul claro
+    elif x > 160 and x < 180 and y < -2 and pygame.mouse.get_pressed()==(1,0,0):
+        line_color = 0, 255, 255
         line_size = 5
 
     #limpar a tela
